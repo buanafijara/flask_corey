2b052bfcb07c9a6153603a0a0ef60d740d941896
Adjie Buanafijar <buanafijara@gmail.com>
2021-08-26 14:37:51 +0700

    part 10 - email and password reset

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -49,11 +49,13 @@ class UpdateAccountForm(FlaskForm):
             user = User.query.filter_by(username=username.data).first()
             if user:
                 raise ValidationError('That username is taken. Please choose a different one.')
+
     def validate_email(self, email):
         if email.data != current_user.email:
-            email = User.query.filter_by(email=email.data).first()
-            if email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
                 raise ValidationError('That email is taken. Please choose a different one.')
+
 
 class PostForm(FlaskForm):
     title = StringField('Title',
@@ -61,3 +63,22 @@ class PostForm(FlaskForm):
     content = TextAreaField('Content',
         validators=[DataRequired()])
     submit = SubmitField('Post')
+
+
+class RequestResetForm(FlaskForm):
+    email = StringField('Email',
+        validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('There is no account with that email. You must register first.')
+    
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password',
+        validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+        validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password')
